453c952b1a77b9b198f14a35c1c1c2c6de698ba2
Dan Smith <dan.smith@theappbusiness.com>
2021-04-05 18:10:39 +0100

    Xcode project template build

---
+++ Appetisers/Appetisers/AppetisersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetisersApp.swift
+//  Appetisers
+//
+//  Created by Dan Smith on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetisersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
